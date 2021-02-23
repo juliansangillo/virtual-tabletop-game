38ea8c76fb7d7afff824d0e8113bac9d4e3c4721
Julian Sangillo <juliansangillo@gmail.com>
2021-02-22 20:39:01 -0500

    feat: Added api to move elements between spaces with input validation

--- Project Virtual Tabletop/Assets/Scripts/Editor/Tests/GameController/SpaceManagerTests.cs
+++ Project Virtual Tabletop/Assets/Scripts/Editor/Tests/GameController/SpaceManagerTests.cs
@@ -284,7 +284,6 @@ namespace ProjectVirtualTabletop.Editor.Tests.GameController {
 			fakeMap[1,0] = null;
 			fakeMap[1,1] = null;
 			spaceManager.Map = fakeMap;
-			Entities.Space from = new Entities.Space(0, 0);
 
 			spaceManager.Move(new Entities.Space(0, 0), new Entities.Space(1, 1));
 			Element actual = spaceManager.Map[1,1];
@@ -292,21 +291,193 @@ namespace ProjectVirtualTabletop.Editor.Tests.GameController {
 			Assert.AreEqual(expected, actual);
 		}
 
-		/*[Test]
-		public void Move_GivenTwoValidSpacesWhereAnElementIsLocatedOnTheSecondSpace_ThrowInvalidOperationException() {
-			Element expected = new Element();
+		[Test]
+		public void Move_GivenTwoValidSpacesWhereAnElementIsLocatedOnTheSecondSpace_ThrowInvalidOperationExceptionWithCorrectMessage() {
+			Element element1 = new Element();
+			Element element2 = new Element();
 			Element[,] fakeMap = new Element[2,2];
-			fakeMap[0,0] = expected;
+			fakeMap[0,0] = element1;
+			fakeMap[0,1] = null;
+			fakeMap[1,0] = null;
+			fakeMap[1,1] = element2;
+			spaceManager.Map = fakeMap;
+
+			Exception expected = new InvalidOperationException("The space to move to is not empty. A space must be unoccupied to add an element to it. " +
+				"Please remove the existing element from this space first before adding another element.");
+
+			Exception actual = Assert.Throws<InvalidOperationException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenTwoValidSpacesWhereTheFirstSpaceIsEmpty_ThrowInvalidOperationExceptionWithCorrectMessage() {
+			Element[,] fakeMap = new Element[2,2];
+			fakeMap[0,0] = null;
 			fakeMap[0,1] = null;
 			fakeMap[1,0] = null;
 			fakeMap[1,1] = null;
 			spaceManager.Map = fakeMap;
-			Entities.Space from = new Entities.Space(0, 0);
 
-			spaceManager.Move(new Entities.Space(0, 0), new Entities.Space(1, 1));
-			Element actual = spaceManager.Map[1,1];
+			Exception expected = new InvalidOperationException("The space to move from is empty. An element must exist on the space to move it");
 
-			Assert.AreEqual(expected, actual);
-		}*/
+			Exception actual = Assert.Throws<InvalidOperationException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenValidFromSpaceWhereRowDoesNotExistOnMap_ThrowArgumentExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space nonExistingSpace = new Entities.Space(5, 1);
+
+			Exception expected = new ArgumentException("The space to move from does not exist on map", "from");
+
+			Exception actual = Assert.Throws<ArgumentException>(() => {
+				spaceManager.Move(nonExistingSpace, new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenValidFromSpaceWhereColumnDoesNotExistOnMap_ThrowArgumentExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space nonExistingSpace = new Entities.Space(1, 5);
+
+			Exception expected = new ArgumentException("The space to move from does not exist on map", "from");
+
+			Exception actual = Assert.Throws<ArgumentException>(() => {
+				spaceManager.Move(nonExistingSpace, new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenValidToSpaceWhereRowDoesNotExistOnMap_ThrowArgumentExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space nonExistingSpace = new Entities.Space(5, 1);
+
+			Exception expected = new ArgumentException("The space to move to does not exist on map", "to");
+
+			Exception actual = Assert.Throws<ArgumentException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), nonExistingSpace);
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenValidToSpaceWhereColumnDoesNotExistOnMap_ThrowArgumentExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space nonExistingSpace = new Entities.Space(1, 5);
+
+			Exception expected = new ArgumentException("The space to move to does not exist on map", "to");
+
+			Exception actual = Assert.Throws<ArgumentException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), nonExistingSpace);
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenInvalidFromSpaceWhereRowIsNegative_ThrowInvalidSpaceExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space invalidSpace = new Entities.Space(-1, 0);
+
+			Exception expected = new InvalidSpaceException("The space to move from is invalid. Please verify neither the row or column " +
+				"are negative and try again.");
+
+			Exception actual = Assert.Throws<InvalidSpaceException>(() => {
+				spaceManager.Move(invalidSpace, new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenInvalidFromSpaceWhereColumnIsNegative_ThrowInvalidSpaceExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space invalidSpace = new Entities.Space(0, -1);
+
+			Exception expected = new InvalidSpaceException("The space to move from is invalid. Please verify neither the row or column " +
+				"are negative and try again.");
+
+			Exception actual = Assert.Throws<InvalidSpaceException>(() => {
+				spaceManager.Move(invalidSpace, new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenInvalidToSpaceWhereRowIsNegative_ThrowInvalidSpaceExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space invalidSpace = new Entities.Space(-1, 0);
+
+			Exception expected = new InvalidSpaceException("The space to move to is invalid. Please verify neither the row or column " +
+				"are negative and try again.");
+
+			Exception actual = Assert.Throws<InvalidSpaceException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), invalidSpace);
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenInvalidToSpaceWhereColumnIsNegative_ThrowInvalidSpaceExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+			Entities.Space invalidSpace = new Entities.Space(0, -1);
+
+			Exception expected = new InvalidSpaceException("The space to move to is invalid. Please verify neither the row or column " +
+				"are negative and try again.");
+
+			Exception actual = Assert.Throws<InvalidSpaceException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), invalidSpace);
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenNullFromSpace_ThrowArgumentNullExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+
+			Exception expected = new ArgumentNullException("from", "The space to move from cannot be null.");
+
+			Exception actual = Assert.Throws<ArgumentNullException>(() => {
+				spaceManager.Move(null, new Entities.Space(1, 1));
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
+
+		[Test]
+		public void Move_GivenNullToSpace_ThrowArgumentNullExceptionWithCorrectMessage() {
+			Element element = new Element();
+			Element[,] fakeMap = new Element[2,2];
+			spaceManager.Map = fakeMap;
+
+			Exception expected = new ArgumentNullException("to", "The space to move to cannot be null.");
+
+			Exception actual = Assert.Throws<ArgumentNullException>(() => {
+				spaceManager.Move(new Entities.Space(0, 0), null);
+			});
+			Assert.AreEqual(expected.Message, actual.Message);
+		}
 	}
 }
--- Project Virtual Tabletop/Assets/Scripts/GameController/SpaceManager.cs
+++ Project Virtual Tabletop/Assets/Scripts/GameController/SpaceManager.cs
@@ -41,6 +41,26 @@ namespace ProjectVirtualTabletop.GameController {
 		}
 
 		public void Move(Space from, Space to) {
+			if(from == null)
+				throw new ArgumentNullException("from", "The space to move from cannot be null.");
+			else if(to == null)
+				throw new ArgumentNullException("to", "The space to move to cannot be null.");
+			else if(!from.IsValid())
+				throw new InvalidSpaceException("The space to move from is invalid. Please verify neither the row or column " +
+					"are negative and try again.");
+			else if(!to.IsValid())
+				throw new InvalidSpaceException("The space to move to is invalid. Please verify neither the row or column " +
+				"are negative and try again.");
+			else if(from.Row >= Map.GetLength(0) || from.Column >= Map.GetLength(1))
+				throw new ArgumentException("The space to move from does not exist on map", "from");
+			else if(to.Row >= Map.GetLength(0) || to.Column >= Map.GetLength(1))
+				throw new ArgumentException("The space to move to does not exist on map", "to");
+			else if(Map[from.Row, from.Column] == null)
+				throw new InvalidOperationException("The space to move from is empty. An element must exist on the space to move it");
+			else if(Map[to.Row, to.Column] != null)
+				throw new InvalidOperationException("The space to move to is not empty. A space must be unoccupied to add an element to it. " +
+					"Please remove the existing element from this space first before adding another element.");
+
 			Map[to.Row, to.Column] = Map[from.Row, from.Column];
 			Map[from.Row, from.Column] = null;
 		}
