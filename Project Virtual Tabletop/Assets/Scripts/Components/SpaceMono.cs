5a52d682954726e1a0f7f972d339f058b1fbf83c
Julian Sangillo <juliansangillo@gmail.com>
2021-02-27 22:17:31 -0500

    feat: Added token and space monos and prototyped movement feature

---
+++ Project Virtual Tabletop/Assets/Scripts/Components/SpaceMono.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Space = ProjectVirtualTabletop.Entities.Space;
+
+public class SpaceMono : MonoBehaviour {
+	[SerializeField] private int row;
+    [SerializeField] private int col;
+
+    private Space space;
+
+    public int Row {
+        set {
+            this.row = value;
+        }
+    }
+    
+    public int Col {
+        set {
+            this.col = value;
+        }
+    }
+    
+    public Space Space {
+        get {
+            return this.space;
+        }
+    }
+    
+    public void Awake() {
+        space = new Space(row, col);
+	}
+}
---
+++ Project Virtual Tabletop/Assets/Scripts/Components/TokenMono.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectVirtualTabletop.Entities;
+using ProjectVirtualTabletop.GameController.Interfaces;
+using UnityEngine;
+using Zenject;
+using Space = ProjectVirtualTabletop.Entities.Space;
+
+public class TokenMono : MonoBehaviour {
+	[SerializeField] private SpaceMono spaceMono;
+    [SerializeField] private int deltaY;
+
+    private Token token;
+    private Vector3 initialPosition;
+
+    private IGridManager gridManager;
+
+    
+
+    public SpaceMono SpaceMono {
+        set {
+            this.spaceMono = value;
+        }
+    }
+
+    public int DeltaY {
+        set {
+            this.deltaY = value;
+        }
+    }
+
+    public Token Token {
+        get {
+            return this.token;
+        }
+    }
+
+    [Inject]
+    public void Construct(IGridManager gridManager) {
+        this.gridManager = gridManager;
+    }
+
+    public void Start() {
+        token = (Token)gridManager.GetElementOn(spaceMono.Space);
+        initialPosition = transform.position;
+    }
+
+    public void OnMouseDown() {
+        transform.Translate(Vector3.up * deltaY);
+    }
+
+    public void OnMouseDrag() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = Physics.RaycastAll(ray).FirstOrDefault(h => h.collider.tag == "Tile");
+
+        if(hit.collider != null) {
+            Transform other = hit.collider.transform;
+            transform.position = new Vector3(other.position.x, transform.position.y, other.position.z);
+        }
+    }
+
+    public void OnMouseUp() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = Physics.RaycastAll(ray).FirstOrDefault(h => h.collider.tag == "Tile");
+
+        if(hit.collider != null) {
+            Space newSpace = hit.collider.GetComponent<SpaceMono>().Space;
+
+            if(gridManager.IsEmpty(newSpace)) {
+                gridManager.Move(token.CurrentSpace, newSpace);
+                token.CurrentSpace = newSpace;
+                transform.Translate(Vector3.down * deltaY);
+                initialPosition = transform.position;
+            }
+            else
+                transform.position = initialPosition;
+        }
+        else
+            transform.position = initialPosition;
+    }
+}
