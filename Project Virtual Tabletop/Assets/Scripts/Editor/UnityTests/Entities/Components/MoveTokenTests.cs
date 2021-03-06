4e78195fd7e283ae28e1bcba52196f3ae7f01e0a
Julian Sangillo <juliansangillo@gmail.com>
2021-03-06 01:03:00 -0500

    feat: Added tests to move token component

---
+++ Project Virtual Tabletop/Assets/Scripts/Editor/UnityTests/Entities/Components/MoveTokenTests.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyBikerGames.ProjectVirtualTabletop.Entities.Components;
+using NaughtyBikerGames.ProjectVirtualTabletop.GridManagement.Interfaces;
+using NaughtyBikerGames.SDK.Editor.UnityTests;
+using NaughtyBikerGames.SDK.Interpolation.Interfaces;
+using NaughtyBikerGames.SDK.Raycast.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NaughtyBikerGames.ProjectVirtualTabletop.Entities;
+using ModestTree.Util;
+using System;
+using NSubstitute.ReturnsExtensions;
+
+namespace NaughtyBikerGames.ProjectVirtualTabletop.Editor.UnityTests.Entities.Components {
+	public class MoveTokenTests : MonobehaviourTests {
+		GameObject tileObject1;
+		GameObject tileObject2;
+        GameObject tokenObject;
+		Token token;
+		MoveToken moveToken;
+
+		IGridManager gridManager;
+		ILerpable lerpable;
+		IRaycastable raycastable;
+
+		[SetUp]
+		public void SetUp() {
+			tileObject1 = new GameObject();
+			tileObject1.transform.position = new Vector3(1, 0, 1);
+			GridSpaceMono gridSpace1 = tileObject1.AddComponent<GridSpaceMono>();
+			gridSpace1.Row = 0;
+			gridSpace1.Col = 0;
+
+			tileObject2 = new GameObject();
+			tileObject2.transform.position = new Vector3(2, 0, 2);
+			GridSpaceMono gridSpace2 = tileObject2.AddComponent<GridSpaceMono>();
+			gridSpace2.Row = 1;
+			gridSpace2.Col = 1;
+
+			token = new Token(gridSpace1.Space);
+
+			tokenObject = new GameObject();
+			tokenObject.transform.position = new Vector3(1, 0, 1);
+			moveToken = tokenObject.AddComponent<MoveToken>();
+			moveToken.CurrentSpace = gridSpace1;
+			moveToken.MaxHeight = 1;
+			moveToken.LerpDuration = 0f;
+
+			gridManager = Substitute.For<IGridManager>();
+			lerpable = Substitute.For<ILerpable>();
+			raycastable = Substitute.For<IRaycastable>();
+
+			moveToken.Construct(gridManager, lerpable, raycastable);
+
+			gridManager.GetElementOn(gridSpace1.Space).Returns(token);
+		}
+
+		[UnityTest]
+		public IEnumerator Start_InitializesTokenAndInitialPositionToCorrectValues() {
+			yield return null;
+
+			Assert.AreEqual(token, moveToken.Token);
+			Assert.AreEqual(tokenObject.transform.position, moveToken.InitialPosition);
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseDown_WhenTriggered_LerpableIsCalledOnceWithCorrectArguments() {
+			Vector3 expectedSource = tokenObject.transform.position;
+			Vector3 expectedDestination = new Vector3(expectedSource.x, 1, expectedSource.z);
+			float expectedLerpDuration = 0f;
+
+			yield return null;
+
+			moveToken.OnMouseDown();
+
+			yield return null;
+
+			lerpable.Received().Lerp(expectedSource, expectedDestination, expectedLerpDuration, Arg.Any<Action<Vector3>>());
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseDrag_WhenTriggered_RaycastableIsCalledOnceWithCorrectArgument() {
+			string expectedTag = "Tile";
+
+			yield return null;
+
+			moveToken.OnMouseDrag();
+
+			yield return null;
+
+			raycastable.Received().CastRayForTag(expectedTag);
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseDrag_CastRayForTagReturnsNullGameObject_DontCallLerp() {
+			raycastable.CastRayForTag("Tile").ReturnsNull();
+			
+			yield return null;
+
+			moveToken.OnMouseDrag();
+
+			yield return null;
+
+			lerpable.DidNotReceive().Lerp(Arg.Any<Vector3>(), Arg.Any<Vector3>(), Arg.Any<float>(), Arg.Any<Action<Vector3>>());
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseDrag_CastRayForTagReturnsATileAndTheTokenIsAlreadyLocatedAtThatTile_DontCallLerp() {
+			raycastable.CastRayForTag("Tile").Returns(tileObject1);
+
+			yield return null;
+
+			moveToken.OnMouseDrag();
+
+			yield return null;
+
+			lerpable.DidNotReceive().Lerp(Arg.Any<Vector3>(), Arg.Any<Vector3>(), Arg.Any<float>(), Arg.Any<Action<Vector3>>());
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseDrag_CastRayForTagReturnsATileAndTheTokenIsNotLocatedAtThatTile_CallLerpToMoveToNewTile() {
+			Vector3 expectedSource = tokenObject.transform.position;
+			Vector3 expectedDestination = new Vector3(tileObject2.transform.position.x, expectedSource.y, tileObject2.transform.position.z);
+			float expectedLerpDuration = 0f;
+			
+			raycastable.CastRayForTag("Tile").Returns(tileObject2);
+
+			yield return null;
+
+			moveToken.OnMouseDrag();
+
+			yield return null;
+
+			lerpable.Received().Lerp(expectedSource, expectedDestination, expectedLerpDuration, Arg.Any<Action<Vector3>>());
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseUp_WhenTriggered_RaycastableIsCalledOnceWithCorrectArgument() {
+			string expectedTag = "Tile";
+
+			yield return null;
+
+			moveToken.OnMouseUp();
+
+			yield return null;
+
+			raycastable.Received().CastRayForTag(expectedTag);
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseUp_GivenTokenWasMovedAndCastRayForTagReturnedNullGameObject_DontCallLerpAndTheTokenPositionSnapsBackToTheInitialPosition() {
+			tokenObject.transform.position = new Vector3(2, 1, 2);
+			Vector3 expected = new Vector3(1, 0, 1);
+
+			raycastable.CastRayForTag("Tile").ReturnsNull();
+
+			yield return null;
+
+			moveToken.OnMouseUp();
+
+			yield return null;
+
+			lerpable.DidNotReceive().Lerp(Arg.Any<Vector3>(), Arg.Any<Vector3>(), Arg.Any<float>(), Arg.Any<Action<Vector3>>());
+			Assert.AreEqual(expected, tokenObject.transform.position);
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseUp_GivenTokenWasMovedAndCastRayForTagReturnedTileThatIsNotEmpty_DontCallLerpAndTheTokenPositionSnapsBackToTheInitialPosition() {
+			tokenObject.transform.position = new Vector3(2, 1, 2);
+			Vector3 expected = new Vector3(1, 0, 1);
+
+			raycastable.CastRayForTag("Tile").Returns(tileObject1);
+			gridManager.IsEmpty(tileObject1.GetComponent<GridSpaceMono>().Space).Returns(false);
+
+			yield return null;
+
+			moveToken.OnMouseUp();
+
+			yield return null;
+
+			lerpable.DidNotReceive().Lerp(Arg.Any<Vector3>(), Arg.Any<Vector3>(), Arg.Any<float>(), Arg.Any<Action<Vector3>>());
+			Assert.AreEqual(expected, tokenObject.transform.position);
+		}
+
+		[UnityTest]
+		public IEnumerator OnMouseUp_GivenTokenWasMovedAndCastRayForTagReturnedTileThatIsEmpty_MoveTheTokenToTheNewSpaceAndCallLerpOnTheCorrectArguments() {
+			tokenObject.transform.position = new Vector3(2, 1, 2);
+			Vector3 expectedSource = tokenObject.transform.position;
+			Vector3 expectedDestination = new Vector3(expectedSource.x, 0, expectedSource.z);
+			float expectedLerpDuration = 0f;
+
+			raycastable.CastRayForTag("Tile").Returns(tileObject2);
+			gridManager.IsEmpty(tileObject2.GetComponent<GridSpaceMono>().Space).Returns(true);
+
+			yield return null;
+
+			moveToken.OnMouseUp();
+
+			yield return null;
+
+			gridManager.Received().Move(tileObject1.GetComponent<GridSpaceMono>().Space, tileObject2.GetComponent<GridSpaceMono>().Space);
+			Assert.AreEqual(tileObject2.GetComponent<GridSpaceMono>().Space, token.CurrentSpace);
+			lerpable.Received().Lerp(expectedSource, expectedDestination, expectedLerpDuration, Arg.Any<Action<Vector3>>());
+		}
+	}
+}
--- Project Virtual Tabletop/Assets/Scripts/Entities/Components/MoveToken.cs
+++ Project Virtual Tabletop/Assets/Scripts/Entities/Components/MoveToken.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Linq;
 using NaughtyBikerGames.ProjectVirtualTabletop.GridManagement.Interfaces;
+using NaughtyBikerGames.SDK.Interpolation.Interfaces;
+using NaughtyBikerGames.SDK.Raycast.Interfaces;
 using UnityEngine;
 using Zenject;
 
 namespace NaughtyBikerGames.ProjectVirtualTabletop.Entities.Components {
 	public class MoveToken : MonoBehaviour {
-        [SerializeField] private GridSpaceMono gridSpaceMono;
+        [SerializeField] private GridSpaceMono currentSpace;
         [SerializeField] private int maxHeight;
-        [SerializeField] [Range(0, 1)] private float lerpDelta;
+        [SerializeField] private float lerpDuration;
 
         private Token token;
         private Vector3 initialPosition;
 
         private IGridManager gridManager;
+        private ILerpable lerpable;
+        private IRaycastable raycastable;
 
-        public GridSpaceMono GridSpaceMono {
+        public GridSpaceMono CurrentSpace {
             set {
-                this.gridSpaceMono = value;
+                this.currentSpace = value;
             }
         }
 
@@ -27,87 +31,74 @@ namespace NaughtyBikerGames.ProjectVirtualTabletop.Entities.Components {
             }
         }
 
-        public float LerpDelta {
+        public float LerpDuration {
             set {
-                this.lerpDelta = value;
+                this.lerpDuration = value;
             }
         }
-
+        
         public Token Token {
             get {
                 return this.token;
             }
         }
 
+        public Vector3 InitialPosition {
+            get {
+                return this.initialPosition;
+            }
+        }
+
         [Inject]
-        public void Construct(IGridManager gridManager) {
+        public void Construct(IGridManager gridManager, ILerpable lerpable, IRaycastable raycastable) {
             this.gridManager = gridManager;
+            this.lerpable = lerpable;
+            this.raycastable = raycastable;
         }
 
         public void Start() {
-            token = (Token)gridManager.GetElementOn(gridSpaceMono.Space);
+            token = (Token)gridManager.GetElementOn(currentSpace.Space);
             initialPosition = transform.position;
         }
 
         public void OnMouseDown() {
-            StartCoroutine(TranslateUp());
-        }
-
-        private IEnumerator TranslateUp() {
-            float lerpPosition = 0f;
-            do {
-                lerpPosition += lerpDelta;
-                if(lerpPosition >= 1f)
-                    lerpPosition = 1f;
-
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, maxHeight, transform.position.z), lerpPosition);
-
-                yield return null;
-            } while(lerpPosition < 1f);
+            Vector3 source = transform.position;
+            Vector3 destination = new Vector3(source.x, maxHeight, source.z);
+            StartCoroutine(lerpable.Lerp(source, destination, lerpDuration, current => transform.position = current));
         }
 
         public void OnMouseDrag() {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = Physics.RaycastAll(ray).FirstOrDefault(h => h.collider.tag == "Tile");
+            GameObject hitObject = raycastable.CastRayForTag("Tile");
+            if(hitObject != null) {
+                Vector3 source = transform.position;
+                Vector3 destination = new Vector3(hitObject.transform.position.x, source.y, hitObject.transform.position.z);
 
-            if(hit.collider != null) {
-                Transform other = hit.collider.transform;
-                transform.position = new Vector3(other.position.x, transform.position.y, other.position.z);
+                if(Vector3.Distance(source, destination) != 0f)
+                    StartCoroutine(lerpable.Lerp(source, destination, lerpDuration, current => transform.position = current));
             }
         }
 
         public void OnMouseUp() {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = Physics.RaycastAll(ray).FirstOrDefault(h => h.collider.tag == "Tile");
-
-            if(hit.collider != null) {
-                GridSpace newSpace = hit.collider.GetComponent<GridSpaceMono>().Space;
+            GameObject hitObject = raycastable.CastRayForTag("Tile");
+            if(hitObject != null) {
+                GridSpace newSpace = hitObject.GetComponent<GridSpaceMono>().Space;
 
                 if(gridManager.IsEmpty(newSpace)) {
                     gridManager.Move(token.CurrentSpace, newSpace);
                     token.CurrentSpace = newSpace;
-                    StartCoroutine(TranslateDown());
+
+                    Vector3 source = transform.position;
+                    Vector3 destination = new Vector3(source.x, 0, source.z);
+                    StartCoroutine(lerpable.Lerp(source, destination, lerpDuration, current => {
+                        transform.position = current;
+                        initialPosition = transform.position;
+                    }));
                 }
                 else
                     transform.position = initialPosition;
             }
             else
                 transform.position = initialPosition;
-        }
-
-        private IEnumerator TranslateDown() {
-            float lerpPosition = 0f;
-            do {
-                lerpPosition += lerpDelta;
-                if(lerpPosition >= 1f)
-                    lerpPosition = 1f;
-
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 0, transform.position.z), lerpPosition);
-
-                yield return null;
-            } while(lerpPosition < 1f);
-
-            initialPosition = transform.position;
         }
     }
 }
